39a33eb9a874ca66148cc552cfe3edc9c62a0fb3
Vitor Baggi <vitor_baggi@hotmail.com>
2016-02-10 00:54:23 -0200

    iniciando branch

--- EXPERIMENTOS/CEH_VBA_v4.m
+++ EXPERIMENTOS/CEH_VBA_v4.m
@@ -1,4 +1,4 @@
-function [I] = CEH_VBA_v4(T, Tn, ITI, dt, valCriterio,sigma)
+function [I] = CEH_VBA_v4(T, Tn, USUST,USUSE, dt, valCriterio,sigma)
 % código readaptado em 02/07/2015	
 % Gets a series of bins with zeros and ones and calculates its entropy
 % Marcelo Bussotti Reyes - 10/2014
@@ -13,7 +13,7 @@ function [I] = CEH_VBA_v4(T, Tn, ITI, dt, valCriterio,sigma)
 
 T = T(T>0);
 Tn = Tn(Tn>0);
-ITI = ITI(ITI>0); 
+USUST = USUST(USUST>0); 
 
 rng = 0:dt:max(T);	
          
@@ -23,37 +23,41 @@ gaussTn = dt/sqrt(2*pi())/sigma*exp(-0.5*((rng-mean(rng))/sigma).^2);
 %Rng para ITI
 
 %colocar 10% da mediana no sigma da gaussiana que alisa a distribuição dos DTs.
-dtITI = (dt*median(ITI));
-rngITI = 0:dtITI:max(ITI);	
-sigmaITI = sigma*median(ITI);
-gaussITI = dtITI/sqrt(2*pi())/sigmaITI*exp(-0.5*((rngITI-mean(rngITI))/sigmaITI).^2);
+dtUSUST = (dt*median(USUST));
+rngUSUST = 0:dtUSUST:max(USUST);	
+sigmaUSUST = sigma*median(USUST);
+gaussUSUST = dtUSUST/sqrt(2*pi())/sigmaUSUST*exp(-0.5*((rngUSUST-mean(rngUSUST))/sigmaUSUST).^2);
+
 
 % Calculates the bins for histogram calculation
 
 countT   = histc(T,rng);		% counting the frequency for each bin
 countTn  = histc(Tn,rng);		% counting the frequency for each bin
-countITI = histc(ITI, rngITI);
-
+countUSUST = histc(USUST, rngUSUST);
+countUSUSE = histc(USUSE, rngUSUST);
 
 % fazendo a convolução das funcões
 d = conv(countT,gaussT,'same');   % convolves the histogram counts with a gaussian for smoothig
 dn = conv(countTn,gaussTn,'same');
-d2 = conv(countITI,gaussITI,'same');   % convolves the histogram counts with a gaussian for smoothig
+d2 = conv(countUSUST,gaussUSUST,'same');   % convolves the histogram counts with a gaussian for smoothig
+d3 = conv(countUSUSE,gaussUSUST,'same');
 
 d = d/length(T)/dt;				% normalizing
 dn = dn/length(Tn)/dt;		
-d2 = d2/length(ITI)/dtITI;
-
+d2 = d2/length(USUST)/dtUSUST;
+d3 = d3/length(USUSE)/dtUSUST;
 
 %valores iguais a zero resultaram em erro no calculo da entropia se nao
 %forem tratatos e excluidos!!!
-d   = d(d>0);	
-dn   = dn(dn>0);	
+d  =  d( d>0);	
+dn = dn(dn>0);	
 d2 = d2(d2>0);
+d3 = d3(d3>0);
 
-entropyT = -dt*sum(d.*log2(d));
-entropyTn = -dt*sum(dn.*log2(dn));
-entropyITI = -dtITI*sum(d2.*log2(d2));
+entropyT     = -dt*sum(d.*log2(d));
+entropyTn    = -dt*sum(dn.*log2(dn));
+entropyUSUST = -dtUSUST*sum(d2.*log2(d2));
+entropyUSUSE = -dtUSUST*sum(d3.*log2(d3));
 
-I = entropyITI - (entropyT + entropyTn);
+I = (entropyUSUST - entropyT) + (entropyUSUSE - entropyTn);
 
--- EXPERIMENTOS/calculadoraDeInformacao.m
+++ EXPERIMENTOS/calculadoraDeInformacao.m
@@ -31,8 +31,8 @@ for n = 1:numeros %numero do rato
 
     matriz = [0 0 0 0 0 0]; %criando matriz 1x6 para pode-la concatenar sem erro
 		clear T; %zerar as tentativas
-		clear USUS	;		
     clear E;
+    
     
 		try
         for s = 1:5 %numero da sessao max 5 			
@@ -68,14 +68,18 @@ for n = 1:numeros %numero do rato
          matriz = matriz(2:length(matriz(:,1)),:);
          %Inclusive a primeira linha de zeros usada para poder mesclar as matrizes some.
          I(n, 3) = matriz(1,5); %Gravando o critério do rato
-         USUS = matriz(1:(length(matriz(:,2))-1),2)+matriz(2:length(matriz(:,1)),1); %Calculando todo intervalo USUS
-         T = matriz(matriz(:,1)>=matriz(1,5)); %Separando reforço positivo
-         E = matriz(matriz(:,1)<matriz(1,5));  %Separando reforço negativo
-				 I(n, 1) = CEH_VBA_v4(T, E, USUS, 0.1, I(n,3),sigma);	%Calculando Informação do rato
-         I(n, 2) = length(matriz(:,1)); 
-			catch
+         USUST = matriz( matriz(:,1) >= matriz(:,5),2 ) + matriz( matriz(:,1) >= matriz(:,5),1 ); %Calculando todo intervalo USUS
+         USUSE = matriz( matriz(:,1) <  matriz(:,5),2 ) + matriz( matriz(:,1) <  matriz(:,5),1 );
+         T = matriz(matriz(:,1)>=matriz(:,5)); %Separando reforço positivo
+         E = matriz(matriz(:,1)<matriz(:,5));  %Separando reforço negativo
+        
+				%disp(USUST)
+        %disp(USUSE)
+      catch
 				disp(strcat('Erro no calculo de informacao. Algum dado falta rato', num2str(n)));
-			end		
-      
+			end	   
+				 
+       I(n, 1) = CEH_VBA_v4(T, E, USUST, USUSE, 0.1, I(n,3),sigma);	%Calculando Informação do rato
+       I(n, 2) = length(matriz(:,1)); 
 	end
-	
+
